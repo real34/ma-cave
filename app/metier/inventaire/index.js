c71b486bf07fea63ab9fb678066035eb937c4d44
Pierre Martin <pierre@occitech.fr>
2016-08-11 00:54:44 +0200

    Remove useless projection

--- app/metier/inventaire/index.js
+++ app/metier/inventaire/index.js
@@ -1,11 +1,10 @@
 import Cave from './projections/cave';
-import Trololo from './projections/trololo';
 
 import LigneOpenCellarImportee from './LigneOpenCellarImportee';
 
 export default {
 	name: 'Inventaire',
 	domainEvents: [LigneOpenCellarImportee],
-	projections: [Cave, Trololo]
+	projections: [Cave]
 	// TODO sideEffects: []
 };
